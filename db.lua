eeb558c31fc563b2e57be20dbbe9127fbc92da6c
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-21 23:08:33 -0400

    Create db.lua

---
+++ db.lua
@@ -0,0 +1,9 @@
+    				_G.savedhumanoidpos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+				game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1039.53931, 21.75, -258.733154)
+				wait(.5)
+
+				fireclickdetector(game.Workspace.Ignored.Shop['[Double-Barrel SG] - $1400'].ClickDetector)
+				fireclickdetector(game.Workspace.Ignored.Shop['[Double-Barrel SG] - $1400'].ClickDetector)
+				wait()
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(_G.savedhumanoidpos)
+end);
